c7c0b785bf9a076a28601852e12e362ee176ffb7
mec_test_1 <mec_test_1@hotmail.com>
2017-01-08 21:09:32 +0800

    correct typo;

--- beginning-node/server.js
+++ beginning-node/server.js
@@ -8,8 +8,7 @@ const serverPath = __dirname;
 const bowerPath = path.join(serverPath, '../bower_components');
 //console.log(bowerPath);
 
-app.use(express.static(serverP
-ath));
+app.use(express.static(serverPath));
 app.use('/bower_components', express.static(bowerPath));
 
 console.log(`serving directiry ${serverPath} with express.js, listening on port ${PORT}`);
