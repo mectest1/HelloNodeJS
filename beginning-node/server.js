cc7b9cc638c867efcea9f48fdcc1babf916fb02d
mec_test_1 <mec_test_1@hotmail.com>
2017-01-08 21:18:51 +0800

    Client side files and server side files should be separated;

--- beginning-node/server.js
+++ beginning-node/server.js
@@ -4,11 +4,11 @@ var path = require('path');
 var app = express();
 const PORT = 3000;
 
-const serverPath = __dirname;
-const bowerPath = path.join(serverPath, '../bower_components');
+const serverPath = path.join(__dirname, './client');
+const bowerPath = path.join(__dirname, '../bower_components');
 //console.log(bowerPath);
 
-app.use(express.static(serverPath));
+app.use('/beginning-node/client', express.static(serverPath));
 app.use('/bower_components', express.static(bowerPath));
 
 console.log(`serving directiry ${serverPath} with express.js, listening on port ${PORT}`);
