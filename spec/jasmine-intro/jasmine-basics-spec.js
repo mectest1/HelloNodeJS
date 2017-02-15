38b0dc36bdbd1ecdf5d78844406773de3c486a4b
mec_test_1 <mec_test_1@hotmail.com>
2017-02-15 20:33:40 +0800

    Play with plain spy(mock) objects;

--- spec/jasmine-intro/jasmine-basics-spec.js
+++ spec/jasmine-intro/jasmine-basics-spec.js
@@ -705,4 +705,43 @@ describe('Introduction to Jasmine', function(){
 			expect(foo.setBar.calls.any()).toBe(false);
 		});
 	});
+	
+	//When there is not a function to spy on, jasmine.createSpy can 
+	//create a "bare" spy. This spy acts as any other spy - tracking calls, arguments, etc.
+	//But ther eis no implementation behind it.
+	//Spies are JavaScript objects and can be used as such.
+	describe('A spy (mock), when created manually', function(){
+		let whatAmI;
+		
+		beforeEach(function(){
+			whatAmI = jasmine.createSpy('whatAmI');
+			
+			whatAmI('I', 'am', 'a', 'spy');
+		});
+		
+		it('is named, which helps in error reporting', function(){
+			expect(whatAmI.and.identity()).toEqual('whatAmI');
+		});
+		
+		it('tracks that the spy wascalled', function(){
+			expect(whatAmI).toHaveBeenCalled();
+		});
+		
+		it('tracks its number of calls', function(){
+			expect(whatAmI.calls.count()).toEqual(1);
+		});
+		
+		it('tracks all the arguments of its calls', function(){
+			expect(whatAmI).toHaveBeenCalledWith('I', 'am', 'a', 'spy');
+		});
+		
+		it('allows access to the most recent call', function(){
+			expect(whatAmI.calls.mostRecent()).args[0].toEqual('I');
+		});
+	});
+	
+	//In order to create a mock with multiple spies, use
+	//jasmine.createSpyObj and pass an array of strings. It returns 
+	//an object that has a property for each string that is a spy.
+	
 });
