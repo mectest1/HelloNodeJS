c8cc0a4f325c9a9a3373cf9fdffa3302929c78f3
mec_test_1 <mec_test_1@hotmail.com>
2017-10-11 13:47:04 +0800

    More HTTP Test Case;

--- profesional_node/test/HttpClientTest.js
+++ profesional_node/test/HttpClientTest.js
@@ -1,12 +1,27 @@
 //#!jasmine
 describe('Test Suite to check the node.http library and make some http requests with it', () => {
 	
-	const http = require('htt');
+	const http = require('http');
 	const fs = require('fs');
 	const path = require('path');
 	const LOCAL_PORT = 8124;
 	
 	it('shows how to make a http GET requeset', done => {
+		const options = {
+			host: 'www.google.com',
+			port: 80,
+			path: '/index.html'
+		};
 		
+		http.get(options, res => {
+			console.log('Got response', res.statusCode);
+			res.setEncoding('utf8');
+			res.on('data', chunk => {
+				console.log('received content from server:');
+				console.log(100 < chunk.length ? chunk.substr(0, 100) + '...' : chunk);
+			}).on('end', () => {
+				done();
+			});
+		});
 	});
 });
